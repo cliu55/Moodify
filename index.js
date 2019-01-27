1a14ac43b88db3ad85c4b57dec58ac6f0ed45b00
Chris Liu <y9t0b@ugrad.cs.ubc.ca>
2019-01-26 21:50:14 -0800

    Added socketio data receiver

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 const app = express()
+const io = require('socket.io-client');
+const socket = io('http://127.0.0.1:4001');
 app.set('port', process.env.PORT || 80);
 app.use(bodyParser.json());
 
@@ -14,6 +16,10 @@ app.post('/', (req, res) => {
     console.log(req.body);
     // res.send('Hello World!')
     res.sendStatus(200);
+});
+
+socket.on("test", data => {
+    console.log(data);
 });
 
 app.listen(app.get('port'), () => {
